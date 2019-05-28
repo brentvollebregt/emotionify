bfef7b85c6310367fdf970510fe4057776127ea4
Brent Vollebregt <volleys2468@gmail.com>
2019-05-28 23:01:17 +1200

    Fixed SPA links in navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,6 +3,8 @@ import logo from './logo.png';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import NavItem from 'react-bootstrap/NavItem'
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation: React.FC = () => {
   return (
@@ -21,9 +23,21 @@ const Navigation: React.FC = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/sort">Sort</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link>
+              <LinkContainer to="/">
+                <NavItem>Home</NavItem>
+              </LinkContainer>
+            </Nav.Link>
+            <Nav.Link>
+              <LinkContainer to="/sort">
+                <NavItem>Sort</NavItem>
+              </LinkContainer>
+            </Nav.Link>
+            <Nav.Link>
+              <LinkContainer to="/about">
+                <NavItem>About</NavItem>
+              </LinkContainer>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
