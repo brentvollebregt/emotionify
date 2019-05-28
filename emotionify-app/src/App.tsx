e9de696424b8e261d31bcb3216b2ffda0af5444e
Brent Vollebregt <volleys2468@gmail.com>
2019-05-28 13:18:38 +1200

    Branding, and making the base project more basic

--- emotionify-app/src/App.tsx
+++ emotionify-app/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <img src={logo} alt="Logo"/>
     </div>
   );
 }
