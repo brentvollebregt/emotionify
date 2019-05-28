ad519ffc9ad9659bf7ea7433ac4f3146b16e7412
Brent Vollebregt <volleys2468@gmail.com>
2019-05-28 16:06:10 +1200

    Setup context Moved navigation to it's own component

---
+++ src/Navigation.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.png';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+const Navigation: React.FC = () => {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="light" variant="light" sticky="top">
+      <Container>
+        <Navbar.Brand href="/">
+          <img 
+            src={logo} 
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Emotionify Logo"
+          />
+          {' Emotionify'}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/sort/">Sort</Nav.Link>
+            <Nav.Link href="/about/">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
