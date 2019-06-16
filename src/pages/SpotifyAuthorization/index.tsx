ade677dd852f827e04da71500c87c166487dd6dc
Brent Vollebregt <volleys2468@gmail.com>
2019-06-16 14:57:32 +1200

    Ability to redirect to a specific page on auth completion

--- src/pages/SpotifyAuthorization/index.tsx
+++ src/pages/SpotifyAuthorization/index.tsx
@@ -8,7 +8,8 @@ import { navigate } from 'hookrouter';
 
 // Based off https://developer.spotify.com/documentation/general/guides/authorization-guide/#implicit-grant-flow
 
-const local_storage_state_key = 'spotify-auth-random-state';
+const localStorageStateKey = 'spotify-auth-random-state';
+const localStorageRedirectKey = 'auth-local-redirect';
 
 interface IProps {
     onTokenChange: (newToken: Token | undefined) => void,
@@ -18,7 +19,16 @@ const SpotifyAuthorization: React.FunctionComponent<IProps> = (props: IProps) =>
     const { onTokenChange } = props;
     const { hash } = window.location;
 
-    const goToHome = () => navigate('/'); // TODO Eventually put this on whatever page we came from (can get the most recent url before we send the user out from the history)
+    const redirectOut = () => {
+        let local_redirect: string | null = localStorage.getItem(localStorageRedirectKey);
+        if (local_redirect === null) {
+            navigate('/');
+        } else {
+            localStorage.removeItem(localStorageRedirectKey);
+            navigate(local_redirect);
+        }
+    }
+    
     const onTokenRecieved = (value: string, expires_in: number) => {
         let expiryDate = new Date();
         expiryDate.setSeconds(expiryDate.getSeconds() + expires_in);
@@ -30,7 +40,7 @@ const SpotifyAuthorization: React.FunctionComponent<IProps> = (props: IProps) =>
     if (hash === '') { // No token in URL, redirect user to request for one
         // Setup random state
         const random_state = randomString(16);
-        localStorage.setItem(local_storage_state_key, random_state);
+        localStorage.setItem(localStorageStateKey, random_state);
         // Redirect
         const url_parameters = {
             'client_id': settings.spotify_client_id,
@@ -54,12 +64,12 @@ const SpotifyAuthorization: React.FunctionComponent<IProps> = (props: IProps) =>
         const state = params.get('state');
 
         if (access_token !== null && expires_in !== null && state !== null) { // All parameters are present
-            const stored_random_state = localStorage.getItem(local_storage_state_key);
+            const stored_random_state = localStorage.getItem(localStorageStateKey);
             
             if (stored_random_state !== null && stored_random_state === state) { // Random state was set and matches
-                localStorage.removeItem(local_storage_state_key);
+                localStorage.removeItem(localStorageStateKey);
                 onTokenRecieved(access_token, parseInt(expires_in));
-                goToHome();
+                redirectOut();
                 message = <>
                     <Spinner animation="border" />
                     <p className="lead">Recieved token</p>
