09671249d118b128c08fd1f096b9dae5cd8bb8ee
Brent Vollebregt <volleys2468@gmail.com>
2019-06-22 18:00:45 +1200

    Cleaned up lines now in another branch

--- src/pages/Sort/PlaylistSelectionTable.tsx
+++ src/pages/Sort/PlaylistSelectionTable.tsx
@@ -19,11 +19,9 @@ const selectedBackground = 'linear-gradient(to right, rgba(0, 82, 157, 0.3), rgb
 const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
     const {playlists, selectedPlaylist, onPlaylistSelected} = props;
 
-    // const [singlePlaylistSelection, setSinglePlaylistSelection] = useState(true);
     const [search, setSearch] = useState('');
     const windowSize = useWindowSize();
 
-    // const singlePlaylistSelectionChange = (value: boolean) => () => setSinglePlaylistSelection(value);
     const onComponentPlaylistSelected = (playlist_id: string) => () => onPlaylistSelected(playlist_id);
     const onSearchChange = (event: React.FormEvent<any>) => setSearch(event.currentTarget.value);
 
@@ -40,16 +38,6 @@ const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
                     <InputGroup.Text>{bootstrapBreakpointBiggerThanSm() ? 'Search Playlists': 'Search'}</InputGroup.Text>
                 </InputGroup.Prepend>
                 <FormControl placeholder="Playlist name..." value={search} onChange={onSearchChange} />
-                {/* <DropdownButton
-                    as={InputGroup.Append}
-                    variant="outline-secondary"
-                    title={singlePlaylistSelection ? 'Single' : 'Multiple'}
-                    id="playlist-selection-types"
-                    alignRight
-                >
-                    <Dropdown.Item onClick={singlePlaylistSelectionChange(true)}>Single Playlist Selection</Dropdown.Item>
-                    <Dropdown.Item onClick={singlePlaylistSelectionChange(false)}>Multiple Playlist Selection</Dropdown.Item>
-                </DropdownButton> */}
             </InputGroup>
 
             <div style={{ maxHeight: 450, overflowX: 'auto' }}>
@@ -61,17 +49,17 @@ const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
                                 onClick={onComponentPlaylistSelected(playlist.id)} 
                                 style={{ cursor: 'pointer', background: selectedPlaylist === playlist.id ? selectedBackground : undefined, display: 'grid', gridTemplateColumns: '60px 1fr' }}
                             >
-                                <div style={{ padding: 2 }}>
+                                <td style={{ padding: 2 }}>
                                     {playlist.images.length > 0 && <img src={playlist.images[0].url} style={{ width: '100%' }} alt={'Artwork for: ' + playlist.name}/>}
-                                </div>
-                                <div style={{ textAlign: 'left', paddingLeft: 10 }}>
+                                </td>
+                                <td style={{ textAlign: 'left', padding: '0 0 0 10px' }}>
                                     <div style={{ fontSize: 22 }}>{playlist.name}</div>
                                     <div>
                                         <Badge variant="primary">{playlist.owner.display_name}</Badge>
                                         <Badge variant="dark" className="ml-1">Songs: {playlist.tracks.total}</Badge>
                                         <Badge variant="danger" className="ml-1">{playlist.public ? 'Public' : 'Private'}</Badge>
                                     </div>
-                                </div>
+                                </td>
                             </tr>
                         )}
                     </tbody>
