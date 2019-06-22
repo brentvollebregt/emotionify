0032df96215f58daf1cb3c169f505cc760d78bc4
Brent Vollebregt <volleys2468@gmail.com>
2019-06-22 16:08:30 +1200

    Fixed up playlist search leading tag formatting on small screens

---
+++ src/hooks/WindowSize.ts
@@ -0,0 +1,36 @@
+// Modified Source: https://github.com/rehooks/window-size
+
+import { useState, useEffect } from 'react';
+
+interface WindowDimensions {
+    innerHeight: number,
+    innerWidth: number,
+    outerHeight: number,
+    outerWidth: number
+}
+
+function getSize(): WindowDimensions {
+    return {
+        innerHeight: window.innerHeight,
+        innerWidth: window.innerWidth,
+        outerHeight: window.outerHeight,
+        outerWidth: window.outerWidth,
+    };
+}
+
+export default function useWindowSize(): WindowDimensions {
+    let [windowSize, setWindowSize] = useState<WindowDimensions>(getSize());
+
+    function handleResize() {
+        setWindowSize(getSize());
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return windowSize;
+}
--- src/pages/Sort/PlaylistSelectionTable.tsx
+++ src/pages/Sort/PlaylistSelectionTable.tsx
@@ -6,6 +6,7 @@ import FormControl from 'react-bootstrap/FormControl';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { PlaylistObjectSimplifiedWithTrackIds } from '../../models/Spotify';
+import useWindowSize from '../../hooks/WindowSize';
 
 interface IProps {
     playlists: PlaylistObjectSimplifiedWithTrackIds[],
@@ -20,6 +21,7 @@ const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
 
     // const [singlePlaylistSelection, setSinglePlaylistSelection] = useState(true);
     const [search, setSearch] = useState('');
+    const windowSize = useWindowSize();
 
     // const singlePlaylistSelectionChange = (value: boolean) => () => setSinglePlaylistSelection(value);
     const onComponentPlaylistSelected = (playlist_id: string) => () => onPlaylistSelected(playlist_id);
@@ -28,13 +30,14 @@ const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
     const filteredPlaylists = playlists.filter(p => p.name.indexOf(search) !== -1 || p.uri.indexOf(search) !== -1);
     const sortedPlaylists = filteredPlaylists.sort((a: PlaylistObjectSimplifiedWithTrackIds, b: PlaylistObjectSimplifiedWithTrackIds) => a.name === b.name ? 0 : a.name > b.name ? 1 : -1);
 
+    const bootstrapBreakpointBiggerThanSm = () => windowSize.innerWidth > 576; // Bootstrap >sm in js
+
     return <>
         <h3 className="mb-3">Select a Playlist</h3>
         <div style={{ width: 700, maxWidth: '100%', margin: 'auto' }}>
             <InputGroup style={{ marginBottom: 5 }}>
                 <InputGroup.Prepend>
-                    <InputGroup.Text className="d-none d-sm-block">Search Playlists</InputGroup.Text>
-                    <InputGroup.Text className="d-sm-none">Search</InputGroup.Text>
+                    <InputGroup.Text>{bootstrapBreakpointBiggerThanSm() ? 'Search Playlists': 'Search'}</InputGroup.Text>
                 </InputGroup.Prepend>
                 <FormControl placeholder="Playlist name..." value={search} onChange={onSearchChange} />
                 {/* <DropdownButton
@@ -42,6 +45,7 @@ const PlaylistSelection: React.FunctionComponent<IProps> = (props: IProps) => {
                     variant="outline-secondary"
                     title={singlePlaylistSelection ? 'Single' : 'Multiple'}
                     id="playlist-selection-types"
+                    alignRight
                 >
                     <Dropdown.Item onClick={singlePlaylistSelectionChange(true)}>Single Playlist Selection</Dropdown.Item>
                     <Dropdown.Item onClick={singlePlaylistSelectionChange(false)}>Multiple Playlist Selection</Dropdown.Item>
