3d727248064d618f5d4b5dbf9dc87562ccc479f8
Brent Vollebregt <brent.vollebregt@gmail.com>
2019-08-31 14:54:07 +1200

    Removed reversal from order by audio feature
    There is already a filter to reverse

--- src/pages/Tools/FilterOrderByAudioFeature.tsx
+++ src/pages/Tools/FilterOrderByAudioFeature.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import InputGroup from 'react-bootstrap/InputGroup';
+import { DropdownButton, Dropdown, InputGroup } from 'react-bootstrap';
 import { FilterFunctionProps } from './filter';
 import { TrackWithAudioFeatures, availableTrackAudioFeatures } from '../../models/Spotify';
 
@@ -11,7 +9,6 @@ const FilterOrderByAudioFeature: React.FunctionComponent<IProps> = (props: IProp
     const { outputCallback} = props;
 
     const [feature, setFeature] = useState('Energy');
-    const [reverse, setReverse] = useState(false);
 
     useEffect(() => {
         const audio_feature_key: keyof SpotifyApi.AudioFeaturesObject = availableTrackAudioFeatures[feature].key;
@@ -19,14 +16,13 @@ const FilterOrderByAudioFeature: React.FunctionComponent<IProps> = (props: IProp
             (tracks: TrackWithAudioFeatures[]): TrackWithAudioFeatures[] => tracks.sort((a: TrackWithAudioFeatures, b: TrackWithAudioFeatures): number => {
                 const a_audio_feature_value: number = a.audio_features === undefined || a.audio_features === null ? 0 : a.audio_features[audio_feature_key] as number;
                 const b_audio_feature_value: number = b.audio_features === undefined || b.audio_features === null ? 0 : b.audio_features[audio_feature_key] as number;
-                return reverse ? a_audio_feature_value - b_audio_feature_value : b_audio_feature_value - a_audio_feature_value;
+                return b_audio_feature_value - a_audio_feature_value;
             }),
-            `Order songs by ${feature}${reverse ? ' (reversed)' : ''}`
+            `Order songs by ${feature}`
         );
-    }, [feature, reverse]);
+    }, [feature]);
 
     const setFeatureFromDropdown = (featureValue: string) => () => setFeature(featureValue);
-    const setReverseFromDropdown = (reverseValue: boolean) => () => setReverse(reverseValue);
 
     return <>
         <InputGroup className="mb-1" style={{ display: 'inline-flex', width: 'auto' }}>
@@ -35,16 +31,6 @@ const FilterOrderByAudioFeature: React.FunctionComponent<IProps> = (props: IProp
             </InputGroup.Prepend>
             <DropdownButton as={InputGroup.Append} id="dropdown-feature" title={feature}>
                 {Object.keys(availableTrackAudioFeatures).map(af => <Dropdown.Item key={af} onClick={setFeatureFromDropdown(af)}>{af}</Dropdown.Item>)}
-            </DropdownButton>
-        </InputGroup>
-
-        <InputGroup className="ml-1" style={{ display: 'inline-flex', width: 'auto' }}>
-            <InputGroup.Prepend>
-                <InputGroup.Text>Reversed</InputGroup.Text>
-            </InputGroup.Prepend>
-            <DropdownButton as={InputGroup.Append} id="dropdown-feature" title={reverse ? 'Yes' : 'No'}>
-                <Dropdown.Item onClick={setReverseFromDropdown(true)}>Yes</Dropdown.Item>
-                <Dropdown.Item onClick={setReverseFromDropdown(false)}>No</Dropdown.Item>
             </DropdownButton>
         </InputGroup>
     </>
