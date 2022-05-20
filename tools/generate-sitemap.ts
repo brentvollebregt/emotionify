9ca96a27ed0bf2a02ef147b184e9dc4972f962aa
Brent Vollebregt <brent.vollebregt@gmail.com>
2022-05-20 23:50:46 +1200

    Add sitemap

---
+++ tools/generate-sitemap.ts
@@ -0,0 +1,17 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import { Readable } from "stream";
+import fs from "fs";
+import config from "../src/config";
+
+const rootDestinations = ["sort", "compare", "tools", "about"];
+
+// An array with your links
+const links = rootDestinations.map((dest) => ({ url: `${dest}`, priority: 0.8 }));
+
+// Create a stream to write to
+const stream = new SitemapStream({ hostname: config.siteUrl });
+
+streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+  fs.writeFileSync("./public/sitemap.xml", data);
+  console.log("Sitemap created.");
+});
