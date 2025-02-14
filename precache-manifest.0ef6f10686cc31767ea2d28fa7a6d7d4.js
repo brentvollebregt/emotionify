b542a7fba1ee20a4fa382d8cc6826bce9f3d5512
brentvollebregt <brentvollebregt@users.noreply.github.com>
2025-02-14 09:37:48 +0000

    deploy: 3444c74f73b0cd1e5958a8671b2f0da195522c21

---
+++ precache-manifest.0ef6f10686cc31767ea2d28fa7a6d7d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a78fa5e2f115cdc2401b490d7b9d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17498f70a4b633530d44",
+    "url": "/static/css/main.5b2a8170.chunk.css"
+  },
+  {
+    "revision": "f6b3fd2621494290a282",
+    "url": "/static/js/2.cf12c687.chunk.js"
+  },
+  {
+    "revision": "17498f70a4b633530d44",
+    "url": "/static/js/main.7d186153.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed08e733d6a6f2ab96ad8af6d15c5aff",
+    "url": "/static/media/banner.ed08e733.png"
+  },
+  {
+    "revision": "5fbd211044c8a5a2ee79cdf62eb0d13f",
+    "url": "/static/media/sort-page-demo.5fbd2110.png"
+  },
+  {
+    "revision": "d154494c9913b044d7a2abb63ee91828",
+    "url": "/static/media/spotify-logo-round.d154494c.png"
+  },
+  {
+    "revision": "9583acbec2d7fd2f37db366cff199eb9",
+    "url": "/static/media/tools-page-demo.9583acbe.png"
+  }
+]);
